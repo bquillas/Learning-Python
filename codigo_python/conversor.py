ec344b39772a4772fa5d85c05713b93d5fa4ff7b
Bill Quillas B <bill.system96@gmail.com>
2020-08-18 18:56:51 -0500

    Conversor de Soles a Dolares

--- codigo_python/conversor.py
+++ codigo_python/conversor.py
@@ -1,10 +1,35 @@
-soles = input("¿cuantos Nuevos soles tienes?: ")
-soles = float(soles)
-
 valor_dolar = 3.5
 
-dolares = soles / valor_dolar
-dolares =round(dolares,2)
-dolares = str(dolares)
+def dolar_soles():
+    soles = input("¿Cuántos Nuevos Soles tienes?: ")
+    soles = float(soles)
 
-print("Tienes $" + dolares + " dolares.")
+    dolares = soles / valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)
+
+    print("Tienes $" + dolares + " dolares.")
+
+def soles_dolar():
+    dolar = input("¿Cuántos Dólares tienes?: ")
+    dolar = float(dolar)
+
+    sol = dolar * valor_dolar
+    sol = round(sol,2)
+    sol = str(sol)
+
+    print("Tienes S./" + sol + " Nuevos Soles.")
+
+
+opcion = input("¿Que cambio desea realizar? -Ingrese Opción 1 o 2- \
+    1. Convertir Dolar a Soles  \
+    2. Convertir Soles a dolares \
+    Opción: ")
+
+opcion = int(opcion)
+
+if (opcion == 1):
+    dolar_soles()
+else:
+    soles_dolar()
+
