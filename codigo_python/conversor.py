16c64fbc9b3110ca092691575b6e0ef7dc03f650
Bill Quillas B <bill.system96@gmail.com>
2020-08-19 12:28:12 -0500

    Conversor de Monedas v.2 concondicionales

--- codigo_python/conversor.py
+++ codigo_python/conversor.py
@@ -1,35 +1,64 @@
-valor_dolar = 3.5
+menu = """
+Bienvenido al conversor de Monedas 💰
 
-def dolar_soles():
+1 - Pesos colombianos a Dolares
+2 - Pesos argentinos a Dolares
+3 - Pesos mexicanos a Dolares
+4 - Nuevos Soles a Dolares
+5 - Dolares a Nuevos Soles
+
+Elige una opción: 
+"""
+
+opcion = input(menu)
+
+if opcion == '1':
+    pesos = input("¿Cuántos Pesos Colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares.")
+
+elif opcion =='2':
+    pesos = input("¿Cuántos Pesos Argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares.")
+
+elif opcion =='3':
+    pesos = input("¿Cuántos Pesos Mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares.")
+
+elif opcion =='4':
     soles = input("¿Cuántos Nuevos Soles tienes?: ")
     soles = float(soles)
-
+    valor_dolar = 3.5
     dolares = soles / valor_dolar
     dolares = round(dolares,2)
     dolares = str(dolares)
 
     print("Tienes $" + dolares + " dolares.")
 
-def soles_dolar():
+elif opcion == '5':
     dolar = input("¿Cuántos Dólares tienes?: ")
     dolar = float(dolar)
-
+    valor_dolar = 3.5
     sol = dolar * valor_dolar
     sol = round(sol,2)
     sol = str(sol)
 
     print("Tienes S./" + sol + " Nuevos Soles.")
 
-
-opcion = input("¿Que cambio desea realizar? -Ingrese Opción 1 o 2- \
-    1. Convertir Dolar a Soles  \
-    2. Convertir Soles a dolares \
-    Opción: ")
-
-opcion = int(opcion)
-
-if (opcion == 1):
-    dolar_soles()
 else:
-    soles_dolar()
+    print("Ingrese una opcion correcta, porfavor.")
 
