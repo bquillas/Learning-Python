c238e1e92dcfe8cc9e199203ef91b38bfd07d550
Bill Quillas B <bill.system96@gmail.com>
2020-08-19 11:59:50 -0500

    Nuevo archivo de Condicionale.py

---
+++ codigo_python/condicionales.py
@@ -0,0 +1,17 @@
+#Shift + alt + A (Para comentar en bloque)
+""" edad = int(input("Escribe tu edad: "))
+if edad > 18:
+    print("Ud. es mayor de edad")
+else:
+    print("Eres menor de edad") """
+
+numero = input("Escribe un numero: ")
+
+numero = int(numero)
+
+if numero > 5:
+    print('Es mayor a 5')
+elif numero == 5:
+    print('Es igual a 5')
+else:
+    print('Es menor a 5')
