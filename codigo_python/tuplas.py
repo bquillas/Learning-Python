02198fdad2e7f7da061c81caaf437772039bab0b
Bill Quillas B <bill.system96@gmail.com>
2020-08-20 11:48:16 -0500

    Clase: Tuplas

---
+++ codigo_python/tuplas.py
@@ -0,0 +1,18 @@
+# TUPLAS
+
+mi_tupla = (1,2,3,4,5)
+
+#Tipo de dato estáico
+#No puedes añadir ni eliminar los datos de la tupla
+#El for funsiona mucho más veloz con tuplas que con listas
+#Estructura de datos más rápida en python
+#Las tuplas son inmutables (el String tambien es inmutable)
+
+
+for elemento in mi_tupla:
+    print(elemento)
+# 1
+# 2
+# 3
+# 4
+# 5
