a889f156e3903b5aa71934717f2ad857eebe8bc8
Bill Quillas B <bill.system96@gmail.com>
2020-08-19 19:21:41 -0500

    Clase: Bucle - FOR

---
+++ codigo_python/for.py
@@ -0,0 +1,19 @@
+""" contador = 1
+
+print(contador)
+
+while contador < 1000:
+    #contador = contador + 1
+    contador += 1
+    print(contador)
+ """
+
+#a = list(range(1000))
+#print(a)
+
+
+""" for contador in range(500,1001): #range recicibe hasta 2 parametros (inicio,fin)
+    print(contador) """
+
+for i in range(10):
+    print(11 * i)
